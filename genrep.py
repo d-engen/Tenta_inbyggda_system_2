f8c615c545fbd206a93d44538e0f08fcf42a0873
David Engen <david.engen722@gmail.com>
2025-10-07 10:52:41 +0200

    fixade bugg

--- genrep.py
+++ genrep.py
@@ -2,15 +2,20 @@
 #Date: 2025-10-07
 
 print('Ei24 - genrep praktiskt prov ht25')
-r = input('Ange resistorer:').split(' ')
-for i in range(len(r)):
-    r[i] = int(r[i])
-serie = 0
-for x in range(len(r)):
-    serie = serie + r[x]
-parallell = 0
-for y in range(len(r)):
-    parallell = parallell + (1/r[y])
-parallell = (1/parallell)
-print(f'Serieresistans: {serie}')
-print(f'Parallellresistans: {parallell}')
+r = input('Ange resistorer:')
+if r == (""):
+    print('Serieresistans:0')
+    print('Parallellresistans:0')
+else:
+    r = r.split(' ')
+    for i in range(len(r)):
+        r[i] = int(r[i])
+    serie = 0
+    for x in range(len(r)):
+        serie = serie + r[x]
+    parallell = 0
+    for y in range(len(r)):
+        parallell = parallell + (1/r[y])
+    parallell = (1/parallell)
+    print(f'Serieresistans: {serie}')
+    print(f'Parallellresistans: {parallell}')
