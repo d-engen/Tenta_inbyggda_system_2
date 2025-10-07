15a49287c8a1f7c657a4e5f61d9a105b7db30aaf
David Engen <david.engen722@gmail.com>
2025-10-07 10:40:05 +0200

    ny kod genrep

---
+++ genrep.py
@@ -0,0 +1,16 @@
+#Author: David Engen
+#Date: 2025-10-07
+
+print('Ei24 - genrep praktiskt prov ht25')
+r = input('Ange resistorer:').split(' ')
+for i in range(len(r)):
+    r[i] = int(r[i])
+serie = 0
+for x in range(len(r)):
+    serie = serie + r[x]
+parallell = 0
+for y in range(len(r)):
+    parallell = parallell + (1/r[y])
+parallell = (1/parallell)
+print(f'Serieresistans: {serie}')
+print(f'Parallellresistans: {parallell}')
